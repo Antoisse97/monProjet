e5ac6d18c38bfabae4354cbd1798179fdf562eb5
jade482 <macieljade18@icloud.com>
2025-11-26 15:30:42 +0100

    piece author

--- Piece.java
+++ Piece.java
@@ -2,8 +2,8 @@
 /**
  * Décrivez votre classe Piece ici.
  *
- * @author (votre nom)
- * @version (un numéro de version ou une date)
+ * @author Sofia Kadiri
+ * @version 1.0
  */
 public class Piece
 {
