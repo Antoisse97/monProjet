30b3c89c193f869463ba9407529db3d0cf4f0a29
Sofia <sofia04.sk@gmail.com>
2025-11-26 16:37:52 +0100

    modif classe emotion

--- Emotion.java
+++ Emotion.java
@@ -5,29 +5,6 @@
  * @author (votre nom)
  * @version (un numéro de version ou une date)
  */
-public class Emotion
-{
-    // variables d'instance - remplacez l'exemple qui suit par le vôtre
-    private int x;
-
-    /**
-     * Constructeur d'objets de classe Emotion
-     */
-    public Emotion()
-    {
-        // initialisation des variables d'instance
-        x = 0;
-    }
-
-    /**
-     * Un exemple de méthode - remplacez ce commentaire par le vôtre
-     *
-     * @param  y   le paramètre de la méthode
-     * @return     la somme de x et de y
-     */
-    public int sampleMethod(int y)
-    {
-        // Insérez votre code ici
-        return x + y;
-    }
+public abstract class Emotion {
+    public abstract void seDeplacer(Robot robot, Monde monde);
 }
